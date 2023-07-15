d06e8f28591f65f7cb9430938b984870eac9bbae
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-07-15 23:09:10 +0530

    feat: added exp section w layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import AboutSection from '@/components/layout/AboutSection';
+import ExperienceSection from '@/components/layout/ExperienceSection';
 import HeaderSection from '@/components/layout/HeaderSection';
 import HeroSection from '@/components/layout/HeroSection';
 import Layout from '@/components/layout/Layout';
@@ -25,12 +26,12 @@ export default function HomePage() {
       <Seo />
 
       <main>
-        <section>
-          <HeaderSection />
-        </section>
+        <HeaderSection />
+
         <section className='container mx-auto px-10 md:px-20'>
           <HeroSection />
           <AboutSection />
+          <ExperienceSection />
         </section>
       </main>
     </Layout>
